cf290d590c77a4675fc263c5cdabbb9bdd3b2b45
李昊 <1097261154@qq.com>
2023-02-09 20:16:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { reactive,ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+const language = ref('zh-cn')
+const noSpace = reactive({
+   autoInsertSpace: false,
+})
+const messageConfig = reactive({
+  max:3
+})
+</script>
+
+<template>
+  <el-config-provider :locale="language" :button="noSpace" :message="messageConfig">
+    <RouterView />
+  </el-config-provider>
+</template>
